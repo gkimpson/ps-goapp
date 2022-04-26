f757ac1fb5981736d266ae7ae7c58a0dcff434f3
Gavin Kimpson <gkimpson@gmail.com>
2022-04-27 00:03:21 +0100

    end of day commit

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
+
+var (
+	name, course = "Gavin K", "Getting started with Go"
+	module, clip = 4, 2
+)
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Name and course are set to", name, "and", course, ".")
+	fmt.Println("Module and clup are set to", module, "and", clip, ".")
+	fmt.Println("Name is of type", reflect.TypeOf(name))
+	fmt.Println("Module is of type", reflect.TypeOf(module))
 }
